f7c499a50d72fa590d943f54dcf5fe211dad5dd7
ivm <stan777.durand777@gmail.com>
2024-08-26 10:22:41 +0200

    update maj setup.py pour gerer arch cuda

--- setup.py
+++ setup.py
@@ -12,13 +12,27 @@ setup(
             sources=['multi_slam/msdp_dpvo/altcorr/correlation.cpp', 'multi_slam/msdp_dpvo/altcorr/correlation_kernel.cu'],
             extra_compile_args={
                 'cxx':  ['-O3'], 
-                'nvcc': ['-O3'],
+                'nvcc': ['-O3',
+                         '-gencode=arch=compute_60,code=sm_60',
+                         '-gencode=arch=compute_61,code=sm_61',
+                         '-gencode=arch=compute_70,code=sm_70',
+                         '-gencode=arch=compute_75,code=sm_75',
+                         '-gencode=arch=compute_80,code=sm_80',
+                         '-gencode=arch=compute_86,code=sm_86',
+                         ],
             }),
         CUDAExtension('cuda_ba',
             sources=['multi_slam/msdp_dpvo/fastba/ba.cpp', 'multi_slam/msdp_dpvo/fastba/ba_cuda.cu', 'multi_slam/msdp_dpvo/fastba/block_e.cu'],
             extra_compile_args={
                 'cxx':  ['-O3'], 
-                'nvcc': ['-O3'],
+                'nvcc': ['-O3'
+                         '-gencode=arch=compute_60,code=sm_60',
+                         '-gencode=arch=compute_61,code=sm_61',
+                         '-gencode=arch=compute_70,code=sm_70',
+                         '-gencode=arch=compute_75,code=sm_75',
+                         '-gencode=arch=compute_80,code=sm_80',
+                         '-gencode=arch=compute_86,code=sm_86',
+                         ],
             },
             include_dirs=[
                 osp.join(ROOT, 'thirdparty/eigen-3.4.0')]
@@ -31,7 +45,16 @@ setup(
                 'multi_slam/msdp_dpvo/lietorch/src/lietorch.cpp', 
                 'multi_slam/msdp_dpvo/lietorch/src/lietorch_gpu.cu',
                 'multi_slam/msdp_dpvo/lietorch/src/lietorch_cpu.cpp'],
-            extra_compile_args={'cxx': ['-O3'], 'nvcc': ['-O3'],}),
+            extra_compile_args={'cxx': ['-O3'], 
+                                'nvcc': ['-O3',
+                                         '-gencode=arch=compute_60,code=sm_60',
+                                         '-gencode=arch=compute_61,code=sm_61',
+                                         '-gencode=arch=compute_70,code=sm_70',
+                                         '-gencode=arch=compute_75,code=sm_75',
+                                         '-gencode=arch=compute_80,code=sm_80',
+                                         '-gencode=arch=compute_86,code=sm_86',
+                                         ],
+                                }),
     ],
     cmdclass={
         'build_ext': BuildExtension
